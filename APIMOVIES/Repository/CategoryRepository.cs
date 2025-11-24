32bfd95d33bb0bb4437f5ce1698ab6377c967ce0
wyJulian <matiasoro100@gmail.com>
2025-11-24 07:58:08 -0500

    Category Repository created

---
+++ APIMOVIES/Repository/CategoryRepository.cs
@@ -0,0 +1,66 @@
+﻿using APIMOVIES.DAL;
+using APIMOVIES.DAL.Models;
+using APIMOVIES.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIMOVIES.Repository
+{ 
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public CategoryRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<bool> CategoryExistById(int id)
+        {
+            return await _context.Categories.AnyAsync(c => c.Id == id);
+        }
+
+        public async Task<bool> CategoryExistByName(string name)
+        {
+            return await _context.Categories.AnyAsync(c => c.Name == name);
+        }
+
+        public async Task<bool> CreateCategoryAsync(Category category)
+        {
+            category.CreatedDate = DateTime.UtcNow;
+            await _context.Categories.AddAsync(category);
+            return await SaveAsync();
+        }
+
+        public async Task<bool> DeleteCategoryAsync(int id)
+        {
+            var category = await GetCategoryAsync(id);
+
+            if ( category == null)
+            {
+                return false;
+            }
+            _context.Categories.Remove(category);
+            return await SaveAsync();
+        }
+
+        public async Task<ICollection<Category>> GetCategoriesAsync()
+        {
+            return await _context.Categories.AsNoTracking().OrderBy(c=>c.Name).ToListAsync();
+        }
+
+        public async Task<Category> GetCategoryAsync(int id)
+        {
+            return await _context.Categories.AsNoTracking().FirstOrDefaultAsync(c=>c.Id==id);
+        }
+
+        public async Task<bool> UpdateCategoryAsync(Category category)
+        {
+            category.ModifiedDate = DateTime.UtcNow;
+            _context.Categories.Update(category);
+            return await SaveAsync();
+        }
+
+        private async Task<bool> SaveAsync()
+        {
+            return await _context.SaveChangesAsync() >= 0 ? true : false;
+        }
+    }
+}
---
+++ APIMOVIES/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using APIMOVIES.DAL.Models;
+
+namespace APIMOVIES.Repository.IRepository
+{
+    public interface ICategoryRepository
+    {
+        Task<ICollection<Category>> GetCategoriesAsync();
+        Task<Category> GetCategoryAsync(int id);
+        Task<bool> CategoryExistById(int id);
+        Task<bool> CategoryExistByName(string name);
+        Task<bool> CreateCategoryAsync(Category category);
+        Task<bool> UpdateCategoryAsync(Category category);
+        Task<bool> DeleteCategoryAsync(int id);
+    }
+}
